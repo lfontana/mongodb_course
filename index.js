8e48e3c0a7e476ffe9eae890d64766e93d56627b
Lou Fontana <loufontana@gmail.com>
2016-03-13 14:25:07 -0600

    Section One Complete

--- index.js
+++ index.js
@@ -1,4 +1,73 @@
-var _ = require('underscore');
-_.each([1, 2, 3], function(v) {
-  console.log(v);
+var mongoose = require('mongoose');
+var schema = require('./schema');
+
+mongoose.connect('mongodb://localhost:27017/test');
+
+// Parameters are: model name, schema, collection name
+var User = mongoose.model('User', schema, 'users');
+
+var user = new User({
+  name: 'John Smith',
+  email: 'john@smith.io'
 });
+
+user.save(function(error) {
+  if (error) {
+    console.log(error);
+    process.exit(1);
+  }
+  User.find({ email: 'john@smith.io' }, function(error, docs) {
+    if (error) {
+      console.log(error);
+      process.exit(1);
+    }
+    console.log(require('util').inspect(docs));
+    process.exit(0);
+  });
+});
+
+
+
+
+
+// Part 1 below
+// var mongodb = require('mongodb');
+// var movies = require('./movies');
+
+// var mongodb = require('mongodb');
+//
+// var uri = 'mongodb://localhost:27017/example';
+// mongodb.MongoClient.connect(uri, function(error, db) {
+//   if (error) {
+//     console.log(error);
+//     process.exit(1);
+//   }
+//
+//   db.collection('sample').insert({ x: 1 }, function(error, result) {
+//     if (error) {
+//       console.log(error);
+//       process.exit(1);
+//     }
+//
+//     db.collection('sample').find().toArray(function(error, docs) {
+//       if (error) {
+//         console.log(error);
+//         process.exit(1);
+//       }
+//
+//       console.log('Found docs:');
+//       docs.forEach(function(doc) {
+//         console.log(JSON.stringify(doc));
+//       });
+//       process.exit(0);
+//     });
+//   });
+// });
+//
+// var fn = require('./myfile.js');
+// fn();
+//
+// var otherFn = require('./test').other;
+// otherFn();
+//
+// console.log("hello");
---
+++ interface.js
@@ -0,0 +1,23 @@
+/*
+ *  Inserts "doc" into the collection "movies".
+ */
+exports.insert = function(db, doc, callback) {
+  db.collection('movies').insert(doc, function(error, result) {
+    if (error) callback(error);
+    callback(null, result);
+  });
+};
+
+
+/*
+ *  Finds all documents in the "movies" collection
+ *  whose "director" field equals the given director,
+ *  ordered by the movie's "title" field. See
+ *  http://mongodb.github.io/node-mongodb-native/2.0/api/Cursor.html#sort
+ */
+exports.byDirector = function(db, director, callback) {
+  db.collection('movies').find({'director': director}).sort({'title':1}).toArray(function(error, items) {
+    if (error) { console.log(error); callback(error);}
+      callback(null, items);
+    });
+  };
